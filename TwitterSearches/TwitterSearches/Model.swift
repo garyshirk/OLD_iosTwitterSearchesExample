f194b53678d0ce4a5054e347ce8c257b984812da
Gary Shirk <gary@mfluent.com>
2015-01-19 21:00:20 -0600

    model updates

--- TwitterSearches/TwitterSearches/Model.swift
+++ TwitterSearches/TwitterSearches/Model.swift
@@ -47,4 +47,114 @@ class Model {
     func synchronize() {
         NSUbiquitousKeyValueStore.defaultStore().synchronize()
     }
+    
+    // return the tag at a specified index
+    func tagAtIndex(index: Int) -> String {
+        return tags[index]
+    }
+    
+    // return query for a given tag
+    func queryForTag(tag: String) -> String? {
+        return searches[tag]
+    }
+    
+    // return the query String for the tag at an index
+    func queryForTagAtIndex(index: Int) -> String? {
+        return searches[tags[index]]
+    }
+    
+    // number of tags
+    var count: Int {
+        return tags.count
+    }
+    
+    // delete tag from tags array and tag-query dictionary,
+    // from user defaults and iCloud key-value pairs
+    func deleteSearchAtIndex(index: Int) {
+        searches.removeValueForKey(tags[index])
+        let removeTag = tags.removeAtIndex(index)
+        updateUserDefaults(updateTags: true, updateSearches: true)
+        
+        // remove search from iCloud
+        let keyValueStore = NSUbiquitousKeyValueStore.defaultStore()
+        keyValueStore.removeObjectForKey(removeTag);
+    }
+    
+    // reorder tags array
+    func moveTagAtIndex(oldIndex: Int, toDestinationIndex newIndex: Int) {
+        let temp = tags.removeAtIndex(oldIndex)
+        tags.insert(temp, atIndex:newIndex);
+        updateUserDefaults(updateTags: true, updateSearches: false)
+    }
+    
+    // update local and iCloud user defaults
+    func updateUserDefaults(# updateTags: Bool, updateSearches: Bool) {
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        
+        if updateTags {
+            userDefaults.setObject(tags, forKey: tagsKey)
+        }
+        
+        if updateSearches {
+            userDefaults.setObject(searches, forKey: pairsKey)
+        }
+        
+        userDefaults.synchronize()
+    }
+    
+    // update searches when iCloud changes occur
+    @objc func updateSearches(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            // check reason for change
+            if let reason = userInfo[NSUbiquitousKeyValueStoreChangeReasonKey] as NSNumber? {
+                
+                // if changes occurred on another device
+                if reason.integerValue == NSUbiquitousKeyValueStoreServerChange ||
+                    reason.integerValue == NSUbiquitousKeyValueStoreInitialSyncChange {
+                        performUpdates(userInfo)
+                }
+            }
+        }
+    }
+    
+    func performUpdates(userInfo: [NSObject : AnyObject?]) {
+        // get changed keys NSArray; conver to [String]
+        let changedKeysObject = userInfo[NSUbiquitousKeyValueStoreChangedKeysKey]
+        let changedKeys = changedKeysObject as [String]
+        
+        // get NSUbiquitousKeyValueStore for updating
+        let keyValueStore = NSUbiquitousKeyValueStore.defaultStore()
+        
+        // update searches based on iCloud changes
+        for key in changedKeys {
+            if let query = keyValueStore.stringForKey(key) {
+                saveQuery(query, forTag: key, syncToCloud: false)
+            } else {
+                searches.removeValueForKey(key)
+                tags = tags.filter{$0 != key}
+                updateUserDefaults(updateTags: true, updateSearches: true);
+            }
+            
+            delegate.modelDataChanged()
+        }
+    }
+    
+    // save a tag-query pair
+    func saveQuery(query: String, forTag tag: String, syncToCloud sync: Bool) {
+        
+        // dictionary method updateValue returns nil if key is new
+        let oldValue = searches.updateValue(query, forKey: tag)
+        
+        if oldValue == nil {
+            tags.insert(tag, atIndex: 0)
+            updateUserDefaults(updateTags: true, updateSearches: true)
+        } else {
+            updateUserDefaults(updateTags: false, updateSearches: true)
+        }
+        
+        // if sync is true, add tag-query pair to iCloud
+        if sync {
+            NSUbiquitousKeyValueStore.defaultStore().setObject(query, forKey: tag)
+        }
+    }
 }
