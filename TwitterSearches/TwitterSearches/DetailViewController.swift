80c4974798074dc48891ec826fa581c4f412ba3d
Gary Shirk <gary@mfluent.com>
2015-01-21 16:56:16 -0600

    detail updates 2

--- TwitterSearches/TwitterSearches/DetailViewController.swift
+++ TwitterSearches/TwitterSearches/DetailViewController.swift
@@ -12,29 +12,27 @@ class DetailViewController: UIViewController {
     
     
     @IBOutlet weak var webView: UIWebView!
-    
-    var detailItem: NSURL?
 
-//    var detailItem: AnyObject? {
-//        didSet {
-//            // Update the view.
-//            self.configureView()
-//        }
-//    }
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
 
-//    func configureView() {
-//        // Update the user interface for the detail item.
-//        if let detail: AnyObject = self.detailItem {
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail: AnyObject = self.detailItem {
 //            if let label = self.detailDescriptionLabel {
 //                label.text = detail.description
 //            }
-//        }
-//    }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //self.configureView()
+        self.configureView()
     }
 
     override func didReceiveMemoryWarning() {
